a47a45c92af11a5119106833d84f6b839b162d1a
Alim <alim.weber@outlook.com>
2024-08-30 10:01:45 +0200

    add scroll and pinch + rotate

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import qr from "qrcode-terminal";
 
 import ip from "ip";
 
+import robot from "robotjs";
+
 let PORT = 4269;
 
 console.log(ip.address())
@@ -19,7 +21,31 @@ let wss = new WebSocketServer({
 	server
 });
 
+let current_mouse_pos = {x : 0, y : 0};
 
+let screen_width = robot.getScreenSize().width;
+
+let sens = 4.3;
+let scroll_sens = 0.2;
+
+
+const clamp = x => {
+
+
+	if(0 < x){
+
+		return x;		
+	}
+
+	if(x <= 0){
+		return 0;
+	}
+
+	if(x >= screen_width){
+		return screen_width;
+	}
+
+}
 
 const WriteStream = process.stdout;
 
@@ -29,10 +55,38 @@ wss.on("connection", (socket) => {
 
 	socket.on("message", msg => {
 
-		msg = msg.toString();
-		
-		console.log(msg);
 
+		msg = JSON.parse(msg.toString());
+
+
+		if(msg.type == "move"){
+
+			current_mouse_pos = {
+				x : clamp(current_mouse_pos.x + msg.x * sens),
+				y : clamp(current_mouse_pos.y  + msg.y * sens)
+			};
+
+			console.log(current_mouse_pos);
+			robot.moveMouseSmooth(current_mouse_pos.x, current_mouse_pos.y, 0.1);	
+		}
+
+		
+		if(msg.type == "tap"){
+			robot.mouseClick();
+
+		}
+
+		if(msg.type == "pinch"){
+			let {rotation, pinch_delta, two_finger_delta} = msg;
+
+			console.log(msg)
+
+
+
+			robot.scrollMouse(two_finger_delta.x * scroll_sens, two_finger_delta.y * scroll_sens);
+
+		}
+		
 
 	})
 
--- static/script.js
+++ static/script.js
@@ -6,31 +6,151 @@ ip = "192.168." + ip1 + "." + ip2;
 let ws = new WebSocket("http://" + ip + ":4269");
 
 let mousepos_on_touch = {x : 0, y:0};
+let last_mouse_pos = {x : 0, y : 0};
 
+
+let polling_rate = 2;
+
+let current_poll = 0;
+
+let touch_start_time = 0;
+
+
+let pinch_pos = []
+let pinch_dist = 0;
+let mid_point = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
 
 	document.body.addEventListener("touchstart", (e) => {
 
-		mousepos_on_touch = {x : e.touches[0].pageX, y: e.touches[0].pageY}
 
+		if(e.targetTouches.length == 1){
+
+			mousepos_on_touch = {x : e.touches[0].pageX, y: e.touches[0].pageY}
+
+
+			touch_start_time = Date.now();
+		}
+
+		if(e.targetTouches.length == 2){
+
+			pinch_pos = [
+				{x : e.touches[0].pageX, y : e.touches[0].pageY},
+				{x : e.touches[1].pageX, y : e.touches[1].pageY}
+			]
+
+			mid_point = get_two_finger_mid_point(pinch_pos[0], pinch_pos[1]);
+	
+
+			pinch_dist = Math.hypot(
+				pinch_pos[0].x - pinch_pos[1].x,
+				pinch_pos[0].y - pinch_pos[1].x
+			)
+
+		}
+
+		if(document.body.requestFullscreen){
+			document.body.requestFullscreen();			
+		}
 	});
+
+	document.body.addEventListener("touchend", (e) => {
+		let touch_end_time = Date.now();
+
+
+		let tap_time = (touch_end_time - touch_start_time);
+
+		if(tap_time < 100){
+			ws.send(JSON.stringify({type:"tap"}))
+		}
+
+	})
 
 	document.body.addEventListener("touchmove", (e) => {
 
+
+		if(current_poll !== polling_rate){
+			current_poll += 1;
+			console.log(current_poll)
+			return
+		} 
+		
+		current_poll = 0;	
+
 		//movement of fingers simple trackpad behav
 		if (e.targetTouches.length == 1){
+		
+
+
 			
+
 			let delta = {
-				x : e.touches[0].pageX/window.innerWidth, 
-				y : e.touches[0].pageY/window.innerHeight, 
+				x : e.touches[0].pageX - mousepos_on_touch.x, 
+				y : e.touches[0].pageY - mousepos_on_touch.y, 
 			}	
 
+			delta.type = "move";
 			ws.send(JSON.stringify(delta))
 
 			mousepos_on_touch = {x : e.touches[0].pageX, y: e.touches[0].pageY}
 
-		}			
+		}	
+
+		if(e.targetTouches.length == 2){
+
+			let current_pinch_pos = [
+				{x : e.touches[0].pageX, y : e.touches[0].pageY},
+				{x : e.touches[1].pageX, y : e.touches[1].pageY}
+			]
+			
+			let current_pinch_dist = Math.hypot(
+				current_pinch_pos[0].x - current_pinch_pos[1].x,
+				current_pinch_pos[0].y - current_pinch_pos[1].x
+			)
+
+			let current_mid_point = get_two_finger_mid_point(current_pinch_pos[0], current_pinch_pos[1]);
+		
+
+			let two_finger_delta = {
+				x : current_mid_point.x - mid_point.x,
+				y : current_mid_point.y - mid_point.y
+			};
+
+			mid_point = current_mid_point;
+
+			let rotation = get_pinch_rotation(current_pinch_pos[0], current_pinch_pos[1])
+
+			ws.send(JSON.stringify({
+				type : "pinch",
+				pinch_delta : current_pinch_dist - pinch_dist,
+				rotation,
+				two_finger_delta,
+			}))
+
+		}
 	})	
 
 })
+
+
+function get_two_finger_mid_point(pos1, pos2){
+	let mid_point = {
+		x : (pos1.x - pos2.x)/2 + pos2.x,	
+		y : (pos1.y - pos2.y)/2 + pos2.y
+	};
+
+	return mid_point;
+}
+
+function get_pinch_rotation(pos1, pos2){
+
+
+	let mid_point = get_two_finger_mid_point(pos1, pos2);
+
+
+	let rotation = Math.atan2(mid_point.y - pos2.y, mid_point.x - pos2.x) * 180/Math.PI;
+
+
+	return rotation
+}
